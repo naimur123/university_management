5211c6a218babf5a63cf4fdc31f770c1e45ec837
Naimur Rahman <70331302+naimur123@users.noreply.github.com>
2023-10-03 16:59:08 +0600

    npm build for cpanel

--- bootstrap/ssr/app2.mjs
+++ bootstrap/ssr/app2.mjs
@@ -1,1 +1,6 @@
-
+import _ from "lodash";
+import "bootstrap";
+import axios from "axios";
+window._ = _;
+window.axios = axios;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
